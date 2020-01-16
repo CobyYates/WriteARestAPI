fd08e5ee0b39fe0edb03663382fb64cfb347db6e
Coby Yates <cobylyates@gmail.com>
2020-01-16 14:17:08 -0700

    working on post, project complete?...

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // write the API here
 
-// res.json({ name: "Coby Yates" })
-
 app.get('/api/data', (req, res) => {
   res.send({ name: "Coby Yates"})
 })
+
+app.post('/api/data', (req, res) => {
+    res.send('test')
+    console.log('test sent')
+})
+
+app.get('/api', (req, res) => {
+    res.send('<h2>You have reached the api</h2>')
+  })
 
 //export app for the tests
 module.exports = app;
--- server.js
+++ server.js
@@ -16,12 +16,3 @@ app.listen(port, function(err){
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to my humble abode</h1>')
 })
-
-app.get('/api', (req, res) => {
-  res.send('<h2>You have reached the api</h2>')
-})
-
-app.post('/api/data', (req, res) => {
-  res.send('test')
-  console.log('test sent')
-})
